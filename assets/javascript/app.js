70583b944867aacff25eb6825b2027cab639aca1
RecoveringChef <54086786+RecoveringChef@users.noreply.github.com>
2019-09-10 03:33:19 -0400

    done homewrk starter with Eric

---
+++ assets/javascript/app.js
@@ -0,0 +1,119 @@
+var card = $("#quiz-area");
+var countStartNumber = 30
+var timer;
+
+var questions = [
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+    
+    {
+        question: "",
+        answers: ["1", "2", "3", "4"] ,
+        correcAnswer: "",
+        image:
+    },
+   
+
+
+]
+
+var game = {
+    question: questions,
+    counter: countStartNumber,
+    currentQuestions: 0,
+    correct: 0,
+    incorrect: 0,
+
+    countdown: function (){
+        //decrement counter
+        //display counter in on page
+        //run timeUp func
+
+    },
+    loadQuestion: function (){
+
+    },
+    
+    nextQuestion: function (){
+
+    },
+    
+    timeUp: function (){
+
+    },
+    
+    results: function (){
+
+    },
+    
+    clicked: function (){
+
+    },
+    answerInCoorectly: function (){
+
+    },
+    
+    answerCorrectly: function (){
+
+    },
+    
+    reset: function (){
+
+    },
+}
